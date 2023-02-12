d8393bdf81d81f2ac1910e899901f34ced84fba4
Boris Sedykh <sedykh.boris@yandex.ru>
2023-02-13 00:27:25 +0300

    Fixed issues after code review

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -27,9 +27,10 @@ namespace tc {
                     return 0;
                 }
                 static const double dr = 3.1415926535 / 180.;
+                static const double radius = 6371000.;
                 return acos(sin(from.lat * dr) * sin(to.lat * dr)
                     + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
-                    * 6371000;
+                    * radius;
             }
 
         }
